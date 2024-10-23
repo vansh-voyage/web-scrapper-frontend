abc00c35bd8c4ed3376391f3b49a8193ed1753d4
Prashant Saini <141322732+prashantSaini01@users.noreply.github.com>
2024-10-23 15:34:37 +0530

    Update config.js

--- src/pages/config.js
+++ src/pages/config.js
@@ -2,7 +2,7 @@
 
 // ECS deployed Environment
 //const API_URL = "http://35.154.171.198:5000";
-const API_URL = "https://scrap-bot-299b.onrender.com"
+const API_URL = "https://scrap-bot-j67k.onrender.com"
 // Local Environment setup
 //const API_URL = "http://127.0.0.1:5000";
 
