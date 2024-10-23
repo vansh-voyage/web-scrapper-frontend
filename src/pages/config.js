9c554a50dbb97caf8a84017db057fdc1a9456d39
Prashant Saini <141322732+prashantSaini01@users.noreply.github.com>
2024-10-23 14:33:37 +0530

    Update config.js

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,5 +1,9 @@
-// config.js
-// const API_URL = "http://35.154.171.198:5000";
-const API_URL = "http://127.0.0.1:5000";
+
+
+// ECS deployed Environment
+const API_URL = "http://35.154.171.198:5000";
+
+// Local Environment setup
+//const API_URL = "http://127.0.0.1:5000";
 
 export default API_URL;
