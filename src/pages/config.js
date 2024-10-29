c6b277227b754457b9908d04e24ded7127444bd5
Prashant Saini <141322732+prashantSaini01@users.noreply.github.com>
2024-10-29 17:53:27 +0530

    Update config.js

--- src/pages/config.js
+++ src/pages/config.js
@@ -2,5 +2,5 @@
 
 // Render Deployed Backend
 //const API_URL = process.env.REACT_APP_API_URL;
-const API_URL = https://scrap-bot-j67k.onrender.com;
+const API_URL = "https://scrap-bot-j67k.onrender.com";
 export default API_URL;
