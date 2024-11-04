80e244a0b8bae459e03af54915247cc6a6fcdd71
vansh <vanshbhardwaj920@gmail.com>
2024-11-04 13:13:41 +0530

    changed config to render

--- src/pages/config.js
+++ src/pages/config.js
@@ -2,7 +2,7 @@
 
 // Render Deployed Backend
 //const API_URL = process.env.REACT_APP_API_URL;
-//const API_URL = "https://scrap-bot-j67k.onrender.com";
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://scrap-bot-j67k.onrender.com";
+// const API_URL = "http://127.0.0.1:5000";
 
 export default API_URL;
